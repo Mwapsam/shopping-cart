af6aa1258316fd8ac8339398b4d23bf88dd55ba3
Mwapsam <mwapsam@gmail.com>
2022-07-06 15:29:08 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'cart', to: 'cart#show'
   post 'cart/add'
   post 'cart/remove'
+  # resources :cart, only: [:show]
   resources :products
   root 'products#index'
 end
